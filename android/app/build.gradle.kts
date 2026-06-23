b662559cf27c5f23f171f0f7b45be1a9d01834ec
zotel1 <zotelsigel@gmail.com>
2026-06-23 16:39:16 -0300

    Permitir alejar más el grafo con InteractiveViewer

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.nodos.frontend_mobile_nodos_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
